78d5a66eb50e393574708ef370c2c75fc1cecc45
minory <olesya.sartina@gmail.com>
2026-04-19 18:30:39 +0700

    fix api

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,5 +1,5 @@
 import type { NewsResponse } from "../types/news";
-export const NEWS_API_URL = import.meta.env.VITE_API_URL;
+export const NEWS_API_URL = import.meta.env.VITE_API_URL || "http://1e14c3489fcb.vps.myjino.ru:5000/api/v1";
 
 interface fetchNewsParams {
   endpoint: string;
